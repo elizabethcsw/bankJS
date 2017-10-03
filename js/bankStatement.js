4e383d1e82867d6d723c0e36c3ee8d357138f905
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-10-03 22:13:25 +0100

    Untangled account from bankStatement

--- js/bankStatement.js
+++ js/bankStatement.js
@@ -1,13 +1,20 @@
 'use strict';
 
 (function(exports) {
-	function bankStatement(bankAccount) {
-		this.bankAccount = bankAccount;
+	function bankStatement(record) {
+		this.record = record;
 		this._header = '\ndate || credit || debit || balance';
 	}
 
+	bankStatement.prototype.print = function() {
+		var details = '';
+		this.sortByDate().forEach(function(hash){
+			details += ( '\n' + hash.date + ' || ' + hash.credit + ' || ' + hash.debit + ' || ' + hash.balance)});
+		return this._header + details;
+	};
+
 	bankStatement.prototype.sortByDate = function() {
-		return this.bankAccount.record.sort(function(a,b){
+		return this.record.sort(function(a,b){
 			a = a.date.split('/');
 			b = b.date.split('/');
 			if (a[1] == b[1]) {
@@ -15,17 +22,8 @@
 			} else if (a[2] == b[2]) {
 				return parseInt(b[1]) - parseInt(a[1])
 			} else if  ( a[2] > b[2] ) { return -1 }
-				else if (a[2] < b[2] ){ return 1 }
+				else if (a[2] < b[2] ) { return 1 }
 		});
-	};
-
-	bankStatement.prototype.print = function() {
-		var details = '';
-		var sorted = this.sortByDate();
-		sorted.forEach(function(hash){
-			details+=( '\n' + hash.date + ' || ' + hash.credit + ' || ' + hash.debit + ' || ' + hash.balance);
-		});
-		return this._header + details;
 	};
 
 	exports.bankStatement = bankStatement;
--- test/bankStatementSpec.js
+++ test/bankStatementSpec.js
@@ -3,9 +3,13 @@ describe('bankStatement', function() {
 	var statement;
 
 	beforeEach(function() {
-		account = {record: [ { date: '10/01/2012', credit: '1000.00', debit: '', balance: '1000.00' }, { date: '13/01/2012', credit: '2000.00', debit: '', balance: '3000.00' }, { date: '14/01/2012', credit: '', debit: '500.00', balance: '2500.00' } ]};
+		account = {record: [
+			{ date: '10/01/2012', credit: '1000.00', debit: '', balance: '1000.00' },
+			{ date: '13/01/2012', credit: '2000.00', debit: '', balance: '3000.00' },
+			{ date: '14/01/2012', credit: '', debit: '500.00', balance: '2500.00' }
+		]};
 
-		statement = new bankStatement(account);
+		statement = new bankStatement(account.record);
 	});
 
 	it('should be in chronological order', function() {
@@ -16,7 +20,11 @@ describe('bankStatement', function() {
 	});
 
 	it('should print in the correct format', function() {
-		expect(statement.print()).toEqual('\ndate || credit || debit || balance\n14/01/2012 ||  || 500.00 || 2500.00\n13/01/2012 || 2000.00 ||  || 3000.00\n10/01/2012 || 1000.00 ||  || 1000.00');
+		expect(statement.print()).toEqual(
+			'\ndate || credit || debit || balance' +
+			'\n14/01/2012 ||  || 500.00 || 2500.00' +
+			'\n13/01/2012 || 2000.00 ||  || 3000.00' +
+			'\n10/01/2012 || 1000.00 ||  || 1000.00' );
 	});
 
 });
