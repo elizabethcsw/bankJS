f7051fa5b0ca0fa8899981767a4a6c4935dbc27a
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-10-03 16:43:57 +0100

    Removed unnecessary spies and added attributes to improved clarity

--- test/bankDepositSpec.js
+++ test/bankDepositSpec.js
@@ -5,17 +5,15 @@ describe('bankDeposit', function() {
 	beforeEach(function() {
 		account = {
 			record: [],
-			addRecord: function(){}
+			balance: function(){ return 2000},
+			addRecord: function(){},
+			addBalance: function(){},
 		};
 
 		deposit = new bankDeposit(account);
 	});
 
 	it('can deposit money into the account', function() {
-		account.addBalance = jasmine.createSpy('addBalance() spy');
-		account.balance = jasmine.createSpy('balance spy').and.returnValue(2000);
-
-
 		deposit.proceed(2000, '2012-01-13');
 		expect(deposit.record).toEqual({ date: '2012-01-13', credit: 2000, debit: '', balance: 2000 });
 	});
--- test/bankWithdrawalSpec.js
+++ test/bankWithdrawalSpec.js
@@ -5,27 +5,25 @@ describe('bankWithdrawal', function() {
 	beforeEach(function() {
 		account = {
 			record: [],
-			addRecord: function(){}
+			balance: function(){ return 1000},
+			addRecord: function(){},
+			addBalance: function(){},
 		};
 		withdrawal = new bankWithdrawal(account);
 
 	});
 
 	it('throws an error when there is insufficient funds to be withdrawn', function() {
-		account.balance = jasmine.createSpy('balance spy').and.returnValue(1000);
 		expect(function() {
 			withdrawal.hasEnoughFunds(3000);
 		}).toThrow('Insufficient funds');
 	});
 
 	it('returns true when there is sufficient funds to be withdrawn', function() {
-		account.balance = jasmine.createSpy('balance spy').and.returnValue(1000);
 		expect(withdrawal.hasEnoughFunds(500)).toEqual(true);
 	});
 
 	it('can withdraw money from the account', function() {
-		account.balance = jasmine.createSpy('balance spy').and.returnValue(1000);
-		account.addBalance = jasmine.createSpy('addBalance() spy');
 		withdrawal.hasEnoughFunds = jasmine.createSpy('hasEnoughFunds() spy').and.returnValue(false);
 
 		withdrawal.proceed(2000, '2012-01-13');
